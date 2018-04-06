592ed6375e6d6e60baa944eb2f675a4f1a4758d9
Ken George <ken@whatever.email>
2018-04-06 12:48:55 -0700

    Adding manifest as index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    batch: "src/batch.js",
+    createLock: "src/createLock.js",
+    deleteLock: "src/deleteLock.js",
+    listLocks: "src/listLocks.js",
+    verifyLocks: "src/verifyLocks.js",
+    verifyObject: "src/verifyObject.js",
+};

c1c9e1f6c03dc99b7f4f99bd4493b107041859e2
Ken George <ken@whatever.email>
2018-04-09 13:12:30 -0700

    Updated manifest format.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 module.exports = {
-    batch: "src/batch.js",
-    createLock: "src/createLock.js",
-    deleteLock: "src/deleteLock.js",
-    listLocks: "src/listLocks.js",
-    verifyLocks: "src/verifyLocks.js",
-    verifyObject: "src/verifyObject.js",
+    manifest: {
+        batch: "src/batch.js",
+        createLock: "src/createLock.js",
+        deleteLock: "src/deleteLock.js",
+        listLocks: "src/listLocks.js",
+        verifyLocks: "src/verifyLocks.js",
+        verifyObject: "src/verifyObject.js",
+        common: "src/common",
+        modules: "src/node_modules"
+    }
 };
